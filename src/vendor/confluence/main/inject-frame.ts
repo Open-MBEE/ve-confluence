08f5bcacc22811826fe1764dd22bec391cbf5ccf
Doris Lam <dlam@jpl.nasa.gov>
2022-11-14 00:39:15 -0800

    don't normalize url to page name/title, just use hash for editor

--- src/vendor/confluence/main/inject-frame.ts
+++ src/vendor/confluence/main/inject-frame.ts
@@ -9,15 +9,7 @@ import {
 
 export const SR_HASH_VE_PAGE_EDIT_MODE = '#editor';
 
-export const P_SRC_EDITOR_SUPPLEMENT = process.env.EDITOR_SUPPLEMENT_SRC || 'https://ced.jpl.nasa.gov/cdn/editor.min.js';
-// process.env.PRODUCTION? 'https://ced-uat.jpl.nasa.gov/cdn/editor.min.js': 'http://localhost:3001/public/build/editor.dev.js';
-
-
-async function microtasks_expire() {
-	for(let i=0; i<128; i++) {
-		await Promise.resolve();
-	}
-}
+export const P_SRC_EDITOR_SUPPLEMENT = process.env.EDITOR_SUPPLEMENT_SRC;
 
 export async function inject_frame(p_href: string): Promise<void> {
 	// mimic loading UI
@@ -46,73 +38,22 @@ export async function inject_frame(p_href: string): Promise<void> {
 			}
 		}
 	}
-
-	// purge 'editor' from hash
-	{
-		const as_parts = new Set(location.hash.slice(1).split(/:/g));
-		as_parts.delete('editor');
-
-		history.replaceState(null, '', `#${[...as_parts].join(':')}`);
-	}
-
-	// let all microtasks die
-	await microtasks_expire();
-
-	// clear all timeouts
-	{
-		let i_timer = window.setTimeout(() => {}, 0);  // eslint-disable-line @typescript-eslint/no-empty-function
-		while(i_timer--) {
-			window.clearTimeout(i_timer);
-		}
-	}
-
-	// clear all intervals
-	{
-		let i_interval = window.setInterval(() => {}, 1000*60*60*24*365);  // eslint-disable-line @typescript-eslint/no-empty-function
-		while(i_interval--) {
-			window.clearInterval(i_interval);
-		}
-	}
-
-	// let all microtasks die
-	await microtasks_expire();
-
-	// clear all script tags
-	document.head.innerHTML = '';
-
-	// // clean up global scope
-	// {
-	// 	reset_window();
-	// }
-
-	// let any remaining microtasks die
-	await microtasks_expire();
-
-	// use special URL to indicate edit mode
-	{
-		const pr_target = location.pathname.replace(/\+*$/, `+++${SR_HASH_VE_PAGE_EDIT_MODE}`);
-
-		// current href does not match target, push to history
-		if(pr_target !== location.pathname+location.search+location.hash) {
-			// history.pushState(null, '', pr_target);
-			history.replaceState(null, '', pr_target);
-		}
-
-		// set iframe target
-		Object.assign(window, {ve4_iframe_target:pr_target});
-	}
-
 	// open, write and close document
 	{
 		document.open();
 		document.write('<html><body><div id="ve4-loading">Loading...</div></body></html>');
 		document.close();
+		window.addEventListener('popstate', (event) => {
+			setTimeout(()=>{
+				location.reload();
+			}); // user hits back button
+		});
 	}
 
 	{
 		const dm_iframe: HTMLIFrameElement = dd('iframe');
 		// hijack iframe href changes
-		
+
 		// upon unload event
 		const fk_unload = () => {
 			// allow URL to change
